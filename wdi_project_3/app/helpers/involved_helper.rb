70fdff9c99cd92ff1e0727b2ba9adf362ff73f08
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-04-27 13:22:10 -0400

    starts API call

--- wdi_project_3/app/helpers/involved_helper.rb
+++ wdi_project_3/app/helpers/involved_helper.rb
@@ -1,2 +1,11 @@
 module InvolvedHelper
+  def primary_name(obj)
+    primary = obj["causes"]["primary"]
+
+    primary["name"] if primary
+  end
+
+  def image_display(obj)
+    obj["image"] || "foo.jpg"
+  end
 end
