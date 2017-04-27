9b8e7b17b69f42f9c7a007d12362f74c4412c596
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-04-27 15:51:30 -0400

    words image

--- wdi_project_3/app/controllers/involved_controller.rb
+++ wdi_project_3/app/controllers/involved_controller.rb
@@ -1,4 +1,13 @@
+
+
 class InvolvedController < ApplicationController
   def index
+    @terms = HTTParty.get("https://www.dosomething.org/api/v1/terms")
+    @campaigns = HTTParty.get("https://www.dosomething.org/api/v1/campaigns?count=250")
+  end
+
+  def show
+    id = params[:id]
+    @campaigns = HTTParty.get("https://www.dosomething.org/api/v1/campaigns?count=250")
   end
 end
--- wdi_project_3/app/helpers/involved_helper.rb
+++ wdi_project_3/app/helpers/involved_helper.rb
@@ -1,2 +1,26 @@
 module InvolvedHelper
+  def primary_name(obj)
+    primary = obj["causes"]["primary"]
+    secondary = obj["causes"]["secondary"]
+    if primary
+      primary["name"]
+    elsif secondary
+      secondary[0]["name"]
+    else
+    end
+  end
+
+  def cause_events(obj)
+    cause_name = primary_name(obj)
+    puts "XXXXX PRINTING BELOW  XXXXXXX"
+    puts cause_name
+    id = params[:id].downcase
+    puts id
+    if cause_name == id
+      obj["title"]
+    else
+      #
+    end
+  end
+
 end
