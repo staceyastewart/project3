85387f168c65be6481f4844ee82c5b2ae25548e7
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-04-27 14:05:05 -0400

    renders pages with correct API info

--- wdi_project_3/app/controllers/involved_controller.rb
+++ wdi_project_3/app/controllers/involved_controller.rb
@@ -2,6 +2,10 @@ class InvolvedController < ApplicationController
   def index
     @terms = HTTParty.get("https://www.dosomething.org/api/v1/terms")
     @campaigns = HTTParty.get("https://www.dosomething.org/api/v1/campaigns?count=250")
+  end
 
+  def show
+    id = params[:id]
+    @campaigns = HTTParty.get("https://www.dosomething.org/api/v1/campaigns?count=250")
   end
 end
--- wdi_project_3/app/helpers/involved_helper.rb
+++ wdi_project_3/app/helpers/involved_helper.rb
@@ -1,11 +1,27 @@
 module InvolvedHelper
   def primary_name(obj)
     primary = obj["causes"]["primary"]
-
-    primary["name"] if primary
+    secondary = obj["causes"]["secondary"]
+    if primary
+      primary["name"]
+    elsif secondary
+      secondary[0]["name"]
+    else
+    end
   end
 
-  def image_display(obj)
-    obj["image"] || "foo.jpg"
+  def cause_events(obj)
+    cause_name = primary_name(obj)
+    puts "XXXXX PRINTING BELOW  XXXXXXX"
+    puts cause_name
+    id = params[:id].downcase
+    puts id
+    if cause_name == id
+      obj["title"]
+
+    else
+      #
+    end
   end
+
 end
