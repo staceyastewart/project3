b129d864f4f2427d4b327a8259fc543e3a703190
Nigel Gardner <nmgardner28@gamil.com>
2017-04-28 14:52:13 -0400

    new save

--- wdi_project_3/app/controllers/events_controller.rb
+++ wdi_project_3/app/controllers/events_controller.rb
@@ -1,9 +1,33 @@
 class EventsController < ApplicationController
   def index
+    @event = Event.all
   end
 
  def show
+  @event = Event.find_by(id: params[:id])
  end
+
+ def edit
+  @event = Event.find(params[:id])
+end
+
+def update
+  @event = Event.find(params[:id])
+  @event.update(
+    title: params[:title],
+    date: params[:date],
+    start_time: params[:date],
+    end_time: params[:date],
+    content: params[:date],
+    topic: params[:topic],
+    price: params[:price],
+    type_of_event: params[:type_of_event],
+    )
+
+  def destroy
+    Event.find(params[:id]).destroy
+    redirect_to "/events"
+  end
 
 end
 
