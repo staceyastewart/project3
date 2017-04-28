14af42d68040cd672fc00de103639148bd2a2723
Kristyn Ginski <Oberst54!>
2017-04-28 15:08:29 -0400

    add api

--- wdi_project_3/db/schema.rb
+++ wdi_project_3/db/schema.rb
@@ -10,25 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426183036) do
+ActiveRecord::Schema.define(version: 20170426182327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.date     "date",       null: false
-    t.time     "start_time", null: false
-    t.time     "end_time"
-    t.text     "content",    null: false
-    t.string   "topic",      null: false
-    t.string   "price"
-    t.string   "type",       null: false
-    t.integer  "user_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -50,5 +35,4 @@ ActiveRecord::Schema.define(version: 20170426183036) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "events", "users"
 end
