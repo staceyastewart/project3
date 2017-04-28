a10af7a2aa3c1ba45986399ef4d7ab361a140244
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-04-28 12:10:24 -0400

    Home page video added

--- wdi_project_3/app/controllers/home_controller.rb
+++ wdi_project_3/app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
   def index
+        @news = HTTParty.get("https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=5045dff729e04b8f898503c8e2f22006")
+    @rand_news= @news["articles"].sample
+
+  end
+
+    def show
+    @news = HTTParty.get("https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=5045dff729e04b8f898503c8e2f22006")
+    @rand_news= @news["articles"].sample
   end
 end
