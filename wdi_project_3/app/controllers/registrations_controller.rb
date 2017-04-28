f22531d417f54b2c6f8a375244c34afddea688be
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-04-28 15:24:03 -0400

    home delete

--- wdi_project_3/app/controllers/registrations_controller.rb
+++ wdi_project_3/app/controllers/registrations_controller.rb
@@ -4,6 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :zip_code, :username)
+
   end
 
   def account_update_params
