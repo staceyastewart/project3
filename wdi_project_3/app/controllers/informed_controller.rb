a78561f2b280f3486c0615cbcda200e2c488960f
Kristyn Ginski <Oberst54!>
2017-04-28 23:34:41 -0400

    add categories

--- wdi_project_3/app/controllers/informed_controller.rb
+++ wdi_project_3/app/controllers/informed_controller.rb
@@ -1,10 +1,37 @@
 class InformedController < ApplicationController
+
   def index
-    client = RedditKit::Client.new('project_active', 'Th1sisn3w86FKA')
-    subreddits = client.subscribed_subreddits
-    @links = client.links('politics')
   end
 
+  def load_links
+    client = RedditKit::Client.new('project_active', 'Th1sisn3w86FKA')
+    if params[:id] == 'animals'
+      reddit = 'ARLAW'
+    elsif params[:id] == 'bullying'
+      reddit = 'bullying'
+    elsif params[:id] == 'disasters'
+      reddit = 'Disasters'
+    elsif params[:id] == 'discrimination'
+      reddit = 'human_rights'
+    elsif params[:id] == 'education'
+      reddit = 'education'
+    elsif params[:id] == 'environment'
+      reddit = 'enviroaction'
+    elsif params[:id] == 'health'
+      reddit = 'globalhealth'
+    elsif params[:id] == 'poverty'
+      reddit = 'poverty'
+    elsif params[:id] == 'relationships'
+      reddit = 'BetterRelationships'
+    else
+      reddit = 'activism'
+    end
+    @links = client.links(reddit).select {|l| l.is_self != true}
+  end
+
+  def show
+    load_links
+  end
 
 
 end
