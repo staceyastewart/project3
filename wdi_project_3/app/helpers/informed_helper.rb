ce7f16b1b018e359335a8760eba5c44d35793923
Kristyn Ginski <Oberst54!>
2017-04-29 00:02:28 -0400

    top stories by category

--- wdi_project_3/app/helpers/informed_helper.rb
+++ wdi_project_3/app/helpers/informed_helper.rb
@@ -1,2 +1,23 @@
 module InformedHelper
+  def category_from_reddit(reddit)
+    if reddit == 'ARLAW'
+      'animals'
+    elsif reddit == 'bullying'
+      'bullying'
+    elsif reddit == 'Disasters'
+      'disasters'
+    elsif reddit == 'human_rights'
+      'discrimination'
+    elsif reddit == 'education'
+      'education'
+    elsif reddit == 'enviroaction'
+      'environment'
+    elsif reddit == 'globalhealth'
+      'health'
+    elsif reddit == 'poverty'
+      'poverty'
+    elsif reddit == 'BetterRelationships'
+      'sex + relationships'
+    end
+  end
 end
