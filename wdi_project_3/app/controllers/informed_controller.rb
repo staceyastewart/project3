2db8b78f963ea2f36c3326fc1f1d989e3d4f57ec
Kristyn Ginski <Oberst54!>
2017-04-28 22:18:32 -0400

    fix api

--- wdi_project_3/app/controllers/informed_controller.rb
+++ wdi_project_3/app/controllers/informed_controller.rb
@@ -1,4 +1,10 @@
 class InformedController < ApplicationController
   def index
+    client = RedditKit::Client.new('project_active', 'Th1sisn3w86FKA')
+    subreddits = client.subscribed_subreddits
+    @links = client.links('politics')
   end
+
+
+
 end
