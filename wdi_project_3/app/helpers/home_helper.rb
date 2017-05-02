7d3ab5e412056f005dc5830d71358b636d1d553b
Laura Amayagit config --global user.email amaya.lauraa@gmail.com <amaya.lauraa@gmail.com>
2017-05-02 18:55:16 -0400

    css profile page

--- wdi_project_3/app/helpers/home_helper.rb
+++ wdi_project_3/app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+    def render_ur(obj)
+    title = obj["name"]
+    title2 = title.gsub(" ", "-")
+    url = "https://www.dosomething.org/us/campaigns/#{title2}"
+    return url
+  end
+
 end
